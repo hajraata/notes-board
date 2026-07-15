2159c6cc3d2d948fc83319402947bdac0d209f7f
hajraata <hajraata@gmail.com>
2026-07-15 18:57:59 -0400

    deployment ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,12 @@
 //setting type as "module" in package.json
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+
 import notesRouter from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
-import cors from "cors";
 
 //setting type as "commonjs" in package.json
 // const express = require("express");
@@ -17,19 +19,31 @@ const app = express();
 
 const PORT = process.env.PORT || 5001;
 
+const __dirname = path.resolve();
+
 // middleware
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-); // allows access /resource sharing from any domain if none is specified
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  ); // allows access /resource sharing from any domain if none is specified
+}
 
 app.use(express.json()); // enables access to the content inside a json/ parses a json
 
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // listening for client requests after connecting to DB
 connectDB().then(() => {
